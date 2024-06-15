8a25cf99b3105dbb2034a7f00f5c2b48ff4a7c2a
Pedro Gregorio <pedro-gregorio@outlook.pt>
2024-06-15 14:03:37 +0100

    Add: Session configurations

--- index.js
+++ index.js
@@ -1,35 +1,42 @@
 const express = require("express");
+const session = require("express-session");
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv').config();
 
 const port = 3000;
 const app = express();
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let extractedParameter;
+app.use(
+  session({
+    secret: process.env.SECRET_KEY, // Replace with a strong secret
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set secure: true if using HTTPS
+  })
+);
 
-app.use("/submit", (req, res, next) => {
+app.post("/submit", (req, res) => {
+  let extractedParameter;
   try {
     let params = new URL(req.body.url).searchParams;
-    if (params.get(req.body.param)) {
-      extractedParameter = params.get(req.body.param);
-    } else {
-      extractedParameter = null;
-    }
+    extractedParameter = params.get(req.body.param) || "";
   } catch (error) {
     console.error(error);
+    extractedParameter = "";
   }
 
+  req.session.extractedParameter = extractedParameter;
   res.redirect("/");
-  next();
 });
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    extractedParameter: extractedParameter
-  });
+  const extractedParameter = req.session.extractedParameter || null;
+  res.render("index.ejs", { extractedParameter: extractedParameter });
 });
 
 app.listen(port, (req, res) => {
